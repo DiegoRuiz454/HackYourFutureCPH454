95951afe4ed15e69cd123a85c2d07a82733d4cf6
DiegoRuiz <differ454@hotmai.com>
2020-11-19 22:20:16 +0100

    Homework react week1 Implemented

---
+++ react/week1/Todo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Atributes from './App.js';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Atributes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
